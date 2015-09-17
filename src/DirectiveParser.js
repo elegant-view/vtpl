aa0631ea336645039f5e8c12ad8dba36c4d601e5
yibuyisheng <yibuyisheng@163.com>
2015-09-17 21:33:39 +0800

    add abstract class

---
+++ src/DirectiveParser.js
@@ -0,0 +1,17 @@
+/**
+ * @file 指令解析器抽象类。指令节点一定是注释节点
+ * @author yibuyisheng(yibuyisheng@163.com)
+ */
+
+var inherit = require('./inherit');
+var Parser = require('./Parser');
+
+function DirectiveParser(options) {
+    Parser.call(this, options);
+}
+
+DirectiveParser.isProperNode = function (node, config) {
+    return node.nodeType === 8;
+};
+
+module.exports = inherit(DirectiveParser, Parser);
