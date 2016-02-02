bdb00011bc3d4266b38256ccc52731e02c9c1f62
yibuyisheng <yibuyisheng@163.com>
2016-02-02 23:31:11 +0800

    inner html

--- src/nodes/Fragment.js
+++ src/nodes/Fragment.js
@@ -3,22 +3,12 @@
  * @author yibuyisheng(yibuyisheng@163.com)
  */
 
+import Node from './Node';
+
 export default class Fragment {
     constructor(manager) {
         this.$$manager = manager;
-
-        let xmlDoc;
-        if (window.DOMParser) {
-            let parser = new DOMParser();
-            xmlDoc = parser.parseFromString('<div></div>', 'text/xml');
-        }
-        // Internet Explorer
-        else {
-            xmlDoc = new ActiveXObject('Microsoft.XMLDOM');
-            xmlDoc.async = false;
-            xmlDoc.loadXML('<div></div>');
-        }
-        this.$$fragment = manager.getNode(xmlDoc.childNodes[0]);
+        this.$$fragment = this.$$manager.createElement('div');
     }
 
     appendChild(node) {
@@ -38,7 +28,54 @@ export default class Fragment {
     }
 
     setInnerHTML(html) {
-        this.$$fragment.setInnerHTML(html);
+        let xmlDoc;
+        if (window.DOMParser) {
+            let parser = new DOMParser();
+            xmlDoc = parser.parseFromString(html, 'text/xml');
+        }
+        // Internet Explorer
+        else {
+            xmlDoc = new ActiveXObject('Microsoft.XMLDOM');
+            xmlDoc.async = false;
+            xmlDoc.loadXML(html);
+        }
+
+        this.$$fragment.setInnerHTML('');
+        walk.call(this, xmlDoc, this.$$fragment);
+
+        function createDOMNode(parserNode) {
+            let nodeType = parserNode.nodeType;
+            if (nodeType === Node.ELEMENT_NODE) {
+                let node = document.createElement(parserNode.tagName);
+                let attributes = parserNode.attributes;
+                for (let i = 0, il = attributes.length; i < il; ++i) {
+                    let attr = attributes[i];
+                    node.setAttribute(attr.name, attr.value);
+                }
+                return this.$$manager.getNode(node);
+            }
+
+            if (nodeType === Node.TEXT_NODE) {
+                let node = document.createTextNode(parserNode.nodeValue);
+                return this.$$manager.getNode(node);
+            }
+
+            if (nodeType === Node.COMMENT_NODE) {
+                let node = document.createComment(parserNode.nodeValue);
+                return this.$$manager.getNode(node);
+            }
+
+            throw new Error(`unknown node type: ${nodeType}`);
+        }
+
+        function walk(rootParserNode, rootDOMNode) {
+            let childNodes = rootParserNode.childNodes;
+            for (let i = 0, il = childNodes.length; i < il; ++i) {
+                let curDOMNode = createDOMNode.call(this, childNodes[i]);
+                rootDOMNode.appendChild(curDOMNode);
+                walk.call(this, childNodes[i], curDOMNode);
+            }
+        }
     }
 
     getInnerHTML() {
