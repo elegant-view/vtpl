9635865cb5699e0d13bea5ddec5dc8bd04318cee
yibuyisheng <yibuyisheng@163.com>
2016-02-29 16:40:44 +0800

    去除掉保留字

--- src/ExprCalculater.js
+++ src/ExprCalculater.js
@@ -10,6 +10,68 @@ export default class ExprCalculater {
     constructor() {
         this.fns = {};
         this.exprNameMap = {};
+
+        this.reservedWords = [
+            'abstract',
+            'boolean',
+            'break',
+            'byte',
+            'case',
+            'catch',
+            'char',
+            'class',
+            'const',
+            'continue',
+            'debugger',
+            'default',
+            'delete',
+            'do',
+            'double',
+            'else',
+            'enum',
+            'export',
+            'extends',
+            'false',
+            'final',
+            'finally',
+            'float',
+            'for',
+            'function',
+            'goto',
+            'if',
+            'implements',
+            'import',
+            'in',
+            'instanceof',
+            'int',
+            'interface',
+            'long',
+            'native',
+            'new',
+            'null',
+            'package',
+            'private',
+            'protected',
+            'public',
+            'return',
+            'short',
+            'static',
+            'super',
+            'switch',
+            'synchronized',
+            'this',
+            'throw',
+            'throws',
+            'transient',
+            'true',
+            'try',
+            'typeof',
+            'var',
+            'void',
+            'volatile',
+            'while',
+            'with'
+        ];
     }
 
     /**
@@ -103,6 +165,18 @@ export default class ExprCalculater {
         for (let variable of possibleVariables) {
             // 如果以数字开头,那就不是变量
             if (!isNaN(parseInt(variable))) {
+                continue;
+            }
+
+            // 如果是javascript保留字,就不是变量
+            let isReservedWord = false;
+            for (let i = 0, il = this.reservedWords.length; i < il; ++i) {
+                if (this.reservedWords[i] === variable) {
+                    isReservedWord = true;
+                    break;
+                }
+            }
+            if (isReservedWord) {
                 continue;
             }
 
