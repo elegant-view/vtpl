07854aa35617ae07494033d266c7a0a40630a79e
yibuyisheng <yibuyisheng@163.com>
2016-07-01 18:39:46 +0800

    dom updater spec

--- test/spec/DomUpdaterSpec.js
+++ test/spec/DomUpdaterSpec.js
@@ -1,49 +1,62 @@
 import DomUpdater from 'src/DomUpdater';
 
 describe('DomUpdater', () => {
-    let domUpdater;
-    beforeEach(() => {
-        domUpdater = new DomUpdater();
+
+    it('should execute task after calling `start` method', done => {
+        const domUpdater = new DomUpdater();
+        const taskId = domUpdater.generateTaskId();
+        let counter = 0;
         domUpdater.start();
-    });
-    afterEach(() => {
-        domUpdater.destroy();
-        domUpdater = null;
-    });
-
-    it('base', done => {
-        let counter = 0;
-        domUpdater.addTaskFn(
-            domUpdater.generateTaskId(),
-            () => {
-                ++counter;
-            },
-            () => {
-                expect(counter).toBe(1);
-                done();
-            }
-        );
-    });
-
-    it('overide', done => {
-        let counter = 0;
-        domUpdater.addTaskFn(
-            domUpdater.generateTaskId(),
-            inc
-        );
-
-        domUpdater.addTaskFn(
-            domUpdater.generateTaskId(),
-            inc
-        );
-
-        setTimeout(() => {
-            expect(counter).toBe(2);
+        domUpdater.addTaskFn(taskId, () => ++counter, () => {
+            expect(counter).toBe(1);
+            domUpdater.destroy();
             done();
-        }, 25);
-
-        function inc() {
-            ++counter;
-        }
+        });
     });
+
+    it('should execute asynchronously', done => {
+        const domUpdater = new DomUpdater();
+        const taskId = domUpdater.generateTaskId();
+        let counter = 0;
+        domUpdater.start();
+        domUpdater.addTaskFn(taskId, () => ++counter, () => {
+            expect(counter).toBe(1);
+            domUpdater.destroy();
+            done();
+        });
+        expect(counter).toBe(0);
+    });
+
+    it('should not execute after calling `destroy` method', done => {
+        const domUpdater = new DomUpdater();
+        const taskId = domUpdater.generateTaskId();
+        let counter = 0;
+        domUpdater.start();
+        domUpdater.addTaskFn(taskId, () => ++counter, () => {
+            done(new Error('wrong'));
+        });
+        domUpdater.destroy();
+        setTimeout(done, 1000);
+    });
+
+    it('should override task function, not notify function', done => {
+        const domUpdater = new DomUpdater();
+        domUpdater.start();
+
+        const taskId = domUpdater.generateTaskId();
+        let taskCounter = 0;
+        let notifyCounter = 0;
+        domUpdater.addTaskFn(taskId, () => ++taskCounter, () => {
+            ++notifyCounter;
+        });
+        domUpdater.addTaskFn(taskId, () => ++taskCounter, () => {
+            ++notifyCounter;
+
+            expect(taskCounter).toBe(1);
+            expect(notifyCounter).toBe(2);
+            domUpdater.destroy();
+            done();
+        });
+    });
+
 });
