76647dc0db6e78791cbef1172f566afaef200712
yibuyisheng <yibuyisheng@163.com>
2016-08-11 23:49:55 +0800

    注释

--- src/Data.js
+++ src/Data.js
@@ -3,15 +3,37 @@
  * @author yibuyisheng(yibuyisheng@163.com)
  */
 
+/**
+ * Data
+ *
+ * @class
+ */
 export default class Data {
+
+    /**
+     * clone
+     *
+     * @public
+     */
     clone() {
         throw new Error('please implement the `clone` method first!');
     }
 
+    /**
+     * equals
+     *
+     * @public
+     */
     equals() {
         throw new Error('please implement the `equal` method first!');
     }
 
+    /**
+     * toString
+     *
+     * @public
+     * @return {string}
+     */
     toString() {
         return '';
     }
